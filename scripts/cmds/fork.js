e876b8d3960775f4d538f18e0cea69fee96f75f4
Saan-Irl <siamexists88@gmail.com>
2026-08-21 07:37:30 +0600

    Upload SAAN BOT V4

---
+++ scripts/cmds/fork.js
@@ -0,0 +1,23 @@
+module.exports = {
+  config: {
+    name: "fork",
+    aliases: ["repo", "source"],
+    version: "2.0",
+    author: "Siam Ahmed Saan",
+    countDown: 3,
+    role: 0,
+    longDescription: "Returns the link to the official repository.",
+    category: "system",
+    guide: { en: "{pn}" }
+  },
+
+  onStart: async function({ message }) {
+    const text = "╭•┈•『 🤍 𝗦𝗮𝗮𝗻-𝐁𝐎𝐓-𝐕3 💖 』•┈•╮\n\n" +
+                 "⚡ 𝐒𝐭𝐚𝐭𝐮𝐬 ──► 𝐀𝐜𝐭𝐢𝐯𝐞 & 𝐒𝐭𝐚𝐛𝐥𝐞\n" +
+                 "👑 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ──► 𝗦𝗶𝗮𝗺 𝗔𝗵𝗺𝗲𝗱 𝗦𝗮𝗮𝗻\n" +
+                 "🔗 𝐑𝐞𝐩𝐨 ────► https://github.com/lazyanik/SATURO-BOT-V2.git\n\n" +
+                 "╰•┈•『 ✓ 𝐊𝐞𝐞𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 ❤️ 』•┈•╯";
+    
+    message.reply(text);
+  }
+};
