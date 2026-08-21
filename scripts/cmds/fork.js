d8fc25f043fb360e02dbe017945b0d344622208b
Saan-Irl <siamexists88@gmail.com>
2026-08-21 07:41:07 +0600

    Update repository link in fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -15,7 +15,7 @@ module.exports = {
     const text = "╭•┈•『 🤍 𝗦𝗮𝗮𝗻-𝐁𝐎𝐓-𝐕3 💖 』•┈•╮\n\n" +
                  "⚡ 𝐒𝐭𝐚𝐭𝐮𝐬 ──► 𝐀𝐜𝐭𝐢𝐯𝐞 & 𝐒𝐭𝐚𝐛𝐥𝐞\n" +
                  "👑 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ──► 𝗦𝗶𝗮𝗺 𝗔𝗵𝗺𝗲𝗱 𝗦𝗮𝗮𝗻\n" +
-                 "🔗 𝐑𝐞𝐩𝐨 ────► https://github.com/lazyanik/SATURO-BOT-V2.git\n\n" +
+                 "🔗 𝐑𝐞𝐩𝐨 ────► https://github.com/Saan-Irl/SAAN-GOAT-BOT-V3.git\n\n" +
                  "╰•┈•『 ✓ 𝐊𝐞𝐞𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 ❤️ 』•┈•╯";
     
     message.reply(text);
